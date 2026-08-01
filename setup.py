08982963bc4b05c32695070a505f859b6606111a
cobra838 <cobra838@gmail.com>
2026-08-01 03:14:34 +0300

    v3.11.0-a3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("docs/README.md", "r") as readme:
 
 setup(
     name="bcml",
-    version="3.11.0-a2",
+    version="3.11.0-a3",
     author="NiceneNerd",
     author_email="macadamiadaze@gmail.com",
     description="A mod manager for The Legend of Zelda: Breath of the Wild",
